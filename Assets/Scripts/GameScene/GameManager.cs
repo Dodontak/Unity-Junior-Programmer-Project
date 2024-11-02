9bdde16f301c915240ed43609abf3bcde82667b3
Dodontak <mu03019@naver.com>
2024-11-02 15:52:23 +0900

    Feat: Display High Score
    - 메인 메뉴에서 하이스코어 볼 수 있도록 구현

--- Assets/Scripts/GameScene/GameManager.cs
+++ Assets/Scripts/GameScene/GameManager.cs
@@ -85,33 +85,3 @@ public class GameManager : MonoBehaviour
         File.WriteAllText(recordFile, json);
     }
 }
-
-[Serializable]
-public class ClearTimeRecord
-{
-    public string playerName;
-    public string clearTime;
-    public ClearTimeRecord(string playerName, TimeSpan clearTime)
-    {
-        this.playerName = playerName;
-        this.clearTime = clearTime.ToString();
-    }
-}
-
-[Serializable]
-public class ClearSpeedRanking
-{ 
-    public List<ClearTimeRecord> ranking;
-    public ClearSpeedRanking()
-    {
-        ranking = new List<ClearTimeRecord>();
-    }
-    public void Add(ClearTimeRecord newRecord)
-    {
-        ranking.Add(newRecord);
-        ranking = ranking
-            .OrderBy(record => TimeSpan.Parse(record.clearTime))
-            .Take(3)
-            .ToList();
-    }
-}
--- Assets/Scripts/MainMenuScene/MainMenuUiHandler.cs
+++ Assets/Scripts/MainMenuScene/MainMenuUiHandler.cs
@@ -4,12 +4,16 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using System;
 
 public class MainMenuUiHandler : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private GameObject mainMenuButtons;
     [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private GameObject[] highScoreRecordPanels;
     private GeneralUIHandler generalUI;
 
     private MainManager mainManager;
@@ -50,5 +54,25 @@ public class MainMenuUiHandler : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    public void FillHighScoreRecord()
+    {
+        string recordFile = Application.persistentDataPath + "/ranking.json";
+        if (!File.Exists(recordFile)) return;
+        string rankingJson = File.ReadAllText(recordFile);
+        List<ClearTimeRecord> rankings = JsonUtility.FromJson<ClearSpeedRanking>(rankingJson).ranking;
+        for (int i = 0; i < rankings.Count; ++i)
+        {
+            FillARankingPanel(highScoreRecordPanels[i], rankings[i]);
+        }
+    }
+
+    private void FillARankingPanel(GameObject rankingPanel, ClearTimeRecord record)
+    {
+        TextMeshProUGUI[] tmps = rankingPanel.GetComponentsInChildren<TextMeshProUGUI>();
+        //index: 0 = Rank string(1st, 2nd..), 1 = name string, 3 = clear time string
+        tmps[1].text = record.playerName;
+        tmps[2].text = TimeSpan.Parse(record.clearTime).Seconds.ToString() + "s";
     }
 }
